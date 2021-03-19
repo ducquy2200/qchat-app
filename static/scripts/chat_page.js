7fc7898546b45b5501ba516900a512328115f7d6
ducquy2200 <ducquy2200@gmail.com>
2021-03-19 15:29:30 -0400

    Add styling for chat sections

--- static/scripts/chat_page.js
+++ static/scripts/chat_page.js
@@ -1,10 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Make 'enter' key submit messages
-  let msg = document.querySelector('#user_message');
-  msg.adEventListener('keyup', event => {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-      document.querySelector('#send_message').click();
-    }
-  })
-})
+
+    // Make sidebar collapse on click
+    document.querySelector('#show-sidebar-button').onclick = () => {
+        document.querySelector('#sidebar').classList.toggle('view-sidebar');
+    };
+
+    // Make 'enter' key submit message
+    let msg = document.getElementById("user_message");
+    msg.addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("send_message").click();
+        }
+    });
+});
