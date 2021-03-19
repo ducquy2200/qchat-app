6ccd61227d5e3ffab9dc6f3f210290d0507ffc73
ducquy2200 <ducquy2200@gmail.com>
2021-03-19 01:30:38 -0400

    Update models.py

--- models.py
+++ models.py
@@ -9,4 +9,4 @@ class User(UserMixin, db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    password = db.Column(db.String(), nullable=False)
+    password = db.Column(db.String(100), unique=False, nullable=False)
