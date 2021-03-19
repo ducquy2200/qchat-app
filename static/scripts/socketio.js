83537f2e1c5cce0d58aff814e9ad165bc7e54bbb
ducquy2200 <ducquy2200@gmail.com>
2021-03-19 15:31:03 -0400

    Add socketio fuctionality

--- static/scripts/socketio.js
+++ static/scripts/socketio.js
@@ -1,66 +1,140 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-  let room = "Lounge";
-  joinRoom("Lounge");
+    // Connect to websocket
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
-  //Display incoming messages
-  socket.on('message', data => {
-    const p = document.createElement('p');
-    const span_username = document.createElement('span');
-    const span_timestamp = document.createElement('span');
-    const br = document.createElement('br');
+    // Retrieve username
+    const username = document.querySelector('#get-username').innerHTML;
 
-    if (data.username){
-      span_username.innerHTML = data.username;
-      span_timestamp.innerHTML = data.time_stamp;
-      p.innerHTML = span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
-      document.querySelector('#display-message-section').append(p);
-    }else{
-      printSysMsg(data.msg);
-    }
-  });
+    // Set default room
+    let room = "Lounge"
+    joinRoom("Lounge");
 
-  // Send message
-  document.querySelector('#send_message').onclick = () => {
-    socket.send({'msg': document.querySelector('#user_message').value, 'username': username , 'room': room});
-    // Clear input already
-    document.querySelector('#user_message').value = '';
-  }
+    // Send messages
+    document.querySelector('#send_message').onclick = () => {
+        socket.emit('incoming-msg', {'msg': document.querySelector('#user_message').value,
+            'username': username, 'room': room});
 
-  // Room selection
-  document.querySelectorAll('.select-room').forEach(p => {
-    p.onclick = () => {
-      let newRoom = p.innerHTML;
-      if (newRoom == room) {
-        msg = `You are already in ${room} room.`;
-        printSysMsg(msg);
-      }else{
+        document.querySelector('#user_message').value = '';
+    };
+
+    // Display all incoming messages
+    socket.on('message', data => {
+
+        // Display current message
+        if (data.msg) {
+            const p = document.createElement('p');
+            const span_username = document.createElement('span');
+            const span_timestamp = document.createElement('span');
+            const br = document.createElement('br')
+            // Display user's own message
+            if (data.username == username) {
+                    p.setAttribute("class", "my-msg");
+
+                    // Username
+                    span_username.setAttribute("class", "my-username");
+                    span_username.innerText = data.username;
+
+                    // Timestamp
+                    span_timestamp.setAttribute("class", "timestamp");
+                    span_timestamp.innerText = data.time_stamp;
+
+                    // HTML to append
+                    p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML
+
+                    //Append
+                    document.querySelector('#display-message-section').append(p);
+            }
+            // Display other users' messages
+            else if (typeof data.username !== 'undefined') {
+                p.setAttribute("class", "others-msg");
+
+                // Username
+                span_username.setAttribute("class", "other-username");
+                span_username.innerText = data.username;
+
+                // Timestamp
+                span_timestamp.setAttribute("class", "timestamp");
+                span_timestamp.innerText = data.time_stamp;
+
+                // HTML to append
+                p.innerHTML += span_username.outerHTML + br.outerHTML + data.msg + br.outerHTML + span_timestamp.outerHTML;
+
+                //Append
+                document.querySelector('#display-message-section').append(p);
+            }
+            // Display system message
+            else {
+                printSysMsg(data.msg);
+            }
+
+
+        }
+        scrollDownChatWindow();
+    });
+
+    // Select a room
+    document.querySelectorAll('.select-room').forEach(p => {
+        p.onclick = () => {
+            let newRoom = p.innerHTML
+            // Check if user already in the room
+            if (newRoom === room) {
+                msg = `You are already in ${room} room.`;
+                printSysMsg(msg);
+            } else {
+                leaveRoom(room);
+                joinRoom(newRoom);
+                room = newRoom;
+            }
+        };
+    });
+
+    // Logout from chat
+    document.querySelector("#logout-btn").onclick = () => {
         leaveRoom(room);
-        joinRoom(newRoom);
-        room = newRoom;
-      }
+    };
+
+    // Trigger 'leave' event if user was previously on a room
+    function leaveRoom(room) {
+        socket.emit('leave', {'username': username, 'room': room});
+
+        document.querySelectorAll('.select-room').forEach(p => {
+            p.style.color = "black";
+        });
     }
-  });
 
-  // Leave room
-  function leaveRoom(room) {
-    socket.emit('leave', {'username' : username, 'room': room});
-  }
+    // Trigger 'join' event
+    function joinRoom(room) {
 
-  // Join room
-  function joinRoom(room) {
-    socket.emit('join', {'username': username, 'room': room});
-    // Clear messages
-    document.querySelector('#display-message-section').innerHTML = ''
-    // Autofocux on text box
-    document.querySelector('#user_message').focus();
-  }
+        // Join room
+        socket.emit('join', {'username': username, 'room': room});
 
-  // Print system message
-  function printSysMsg(msg) {
-    const p = document.createElement('p');
-    p.innerHTML = msg;
-    document.querySelector('#display-message-section').append(p);
-  }
-})
+        // Highlight selected room
+        document.querySelector('#' + CSS.escape(room)).style.color = "#ffc107";
+        document.querySelector('#' + CSS.escape(room)).style.backgroundColor = "white";
+
+        // Clear message area
+        document.querySelector('#display-message-section').innerHTML = '';
+
+        // Autofocus on text box
+        document.querySelector("#user_message").focus();
+    }
+
+    // Scroll chat window down
+    function scrollDownChatWindow() {
+        const chatWindow = document.querySelector("#display-message-section");
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
+
+    // Print system messages
+    function printSysMsg(msg) {
+        const p = document.createElement('p');
+        p.setAttribute("class", "system-msg");
+        p.innerHTML = msg;
+        document.querySelector('#display-message-section').append(p);
+        scrollDownChatWindow()
+
+        // Autofocus on text box
+        document.querySelector("#user_message").focus();
+    }
+});
